f90ecc2b113c5bd362c78e1d552caf686f330538
kryss88ltj <christine.stauffer@gmail.com>
2014-06-15 15:17:20 -0400

    updated sliders

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
 
 
 <div class="main-wrap">
-    <section class="section1">
+
+    <!-- Section 1 -->
+    <section class="section1 clearfix">
       <img class="eye" src="assets/img/home/eye.gif" alt="eye logo">
 
       <div class="text-wrap">
-        <h1 class="everything">everything</h1>
+        <h1 class="everything">ever<span class="y">y</span>thin<span class="g">g</span></h1>
         <h2 class="about">I know about</h2>
         <h1 class="color">color</h1>
         <h6 class="learned">(i learned from sylvia)</h6> 
@@ -20,9 +22,40 @@
         <a href="#"><img src="assets/img/home/mid-arrow.png" alt="mid arrow"></a>
         <a href="#"><img src="assets/img/home/small-arrow.png" alt="small arrow"></a>
       </div>
-
     </section>
-  </div>
+
+    <!-- Section 2 -->
+    <section class="section2 clearfix">
+      <h1 class="sylvia-title">SYLVIA GAFFNEY: MASTER OF COLOR</h1> 
+      <p class="sylvia-text">I thought I had gleaned a good grasp on the concepts in my years as a &#34;designer&#34; - I use that word loosely, as I&#39;d had no training and only a moderate understanding of design.  But only in meeting a true master could I understand my amateur status. I was blind to the colors all around me, even in the whitest whites and deepest blacks. This collection is only a snippet of what she teaches every art director and designer at the Creative Circus, should they be so lucky to learn from the best. And if you&#39;re a rising designer, reading these simple cliffnotes before you take color theory, <span class="bold">How can you live with yourself?</span></p>
+      <img class="sylvia-img" src="assets/img/home/sylvia.png" alt="sylvia">
+    </section>
+
+    <!-- Section 3 -->
+    <section class="section3 clearfix">
+      <h1 class="bourges-title">THE BOURGES COLOR WHEEL</h1> 
+      <p class="bourges-text">The Bourges color chart was developed by Jean Bourges in the mid-1900s in an attempt to redefine the way we classify and use color. She debuted her system in Color Bytes and explained that she intended to expand on Albert Munsell&#39;s 10-sectio<span class="n">n</span> system to explore color from a myriad of different angles and perspectives. Bourges was one of Sylvia&#39;s most significant influences, in addition to Dewey Sadka and the Pope, who surely wears a hat.</p>
+      <img class="bourges-img" src="assets/img/home/section3eye.png" alt="eye-icon">
+    </section>
+
+    <!-- Section 4 -->
+    <section class="4">
+      <div class="color-text">
+        <div class="primary">
+          <h5>PRIMARY COLORS</h5>
+          Alright nerds, these are the colors you learned while finger painting your way through pre-school. RED, YELLOW &#38; BLUE, the basis of all colors.
+        </div>
+        <div class="secondary">
+          <h5>SECONDARY COLORS</h5>
+          In the world of paints, your RYB primaries will combine into  ORANGE, GREEN &#38; VIOLET. But what about when you don’t mix your primaries evenly? You end up with one of the...
+        </div>
+        <div class="tertiary">
+          <h5>TERTIARY</h5>
+          As you combine more of one primary color than another into your secondary color, we move into tertiary families, RED-ORANGE,  YELLOW-ORANGE, YELLOW-GREEN, BLUE-GREEN, BLUE-VIOLET &#38; RED-VIOLET.
+        </div>
+      </div> 
+    </section>
+  </div> <!-- end main wrap --> 
 
 
 <?php include("footer.php"); ?>
--- mauve.php
+++ mauve.php
@@ -3,122 +3,127 @@
 
 <?php include("header.php"); ?>
 
-<!-- Side Menu Left -->
-<nav class="left-nav">
-  <a href="magenta.php">
-    <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-    <h5>REGINA GEORGE IN COLOR FORM</h5>
-    <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-    <h4>magenta</h4>
-    <img class="icon" src="assets/img/nav/magenta-icon.png" alt="magenta icon">
-  </a>
-</nav>
 
-<!-- Side Menu Right -->
-<nav class="right-nav">
-  <a href="magenta.php">
-    <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-    <h5>REGINA GEORGE IN COLOR FORM</h5>
-    <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-    <h4>magenta</h4>
-    <img class="icon" src="assets/img/nav/magenta-icon.png" alt="magenta icon">
-  </a>
-</nav>
+<!-- Main Section - Includes all but bottom nav slider -->
+<section class="main-section clearfix">
 
- <!-- Color Title --> 
-  <h1 class="title">mauve</h1>
+  <!-- Side Menu Left -->
+  <nav class="left-nav">
+    <a href="magenta.php">
+      <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h5>REGINA GEORGE IN COLOR FORM</h5>
+      <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h4>magenta</h4>
+      <img class="icon" src="assets/img/nav/magenta-icon.png" alt="magenta icon">
+    </a>
+  </nav>
 
-  <!-- Bourges Number --> 
-  <div class="bourgesBlock">
-    <h6 class="bourges">bourges</h6>
-    <h5 class="numeral">1</h5>
-    <h6 class="number">number</h6>
-  </div> 
+  <!-- Side Menu Right -->
+  <nav class="right-nav">
+    <a href="magenta.php">
+      <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h5>REGINA GEORGE IN COLOR FORM</h5>
+      <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h4>magenta</h4>
+      <img class="icon" src="assets/img/nav/magenta-icon.png" alt="magenta icon">
+    </a>
+  </nav>
 
-  <!-- Percentage Bar --> 
-  <section class="percentageBar">
-    <div class="top-border"></div>
-    <div class="ninety">90%</div>
-    <div class="eighty">80%</div>
-    <div class="seventy">70%</div>
-    <div class="sixty">60%</div>
-    <div class="fifty">50%</div>
-    <div class="forty">40%</div>
-    <div class="thirty">30%</div>
-    <div class="twenty">20%</div>
-    <div class="ten">10%</div>
-    <div class="five">5%</div>
-  </section> 
+   <!-- Color Title --> 
+    <h1 class="title">mauve</h1>
 
-  <!-- Description Bar --> 
-  <section class="desc-bar collapsed">
-    <img src="assets/img/line.png" alt="lines" class="line1">
-    <p class="desc-text">Mauve is also called Analine Purple, Mauvaline and Mallow</p>
-    <img src="assets/img/line.png" alt="lines" class="line2">
-    <div class="down-arrow"></div>
-  </section>
+    <!-- Bourges Number --> 
+    <div class="bourgesBlock">
+      <h6 class="bourges">bourges</h6>
+      <h5 class="numeral">1</h5>
+      <h6 class="number">number</h6>
+    </div> 
 
-  
-  <section class="icons">
-    <!-- Info Icons - Top -->
-    <section class="top-icons clearfix">
-      <div>
-        <img class="top-icon1" src="assets/img/mauve/icons/icon1.png" alt="">
-        <img class="top-icon2"src="assets/img/mauve/icons/icon2.png" alt="">
-      </div>
-        <p class="icon-text left-text1">first chem color</p>
-        <p class="icon-text right-text1">stylish</p>
-      <div>
-      </div>
-       <div>
-        <img class="top-icon3" src="assets/img/mauve/icons/icon3.png" alt="">
-        <img class="top-icon4" src="assets/img/mauve/icons/icon4.png" alt="">
-      </div>
-      <div>
-        <p class="icon-text left-text2">mournful</p>
-        <p class="icon-text right-text2">expensive</p>
-      </div>
+    <!-- Percentage Bar --> 
+    <section class="percentageBar">
+      <div class="top-border"></div>
+      <div class="ninety">90%</div>
+      <div class="eighty">80%</div>
+      <div class="seventy">70%</div>
+      <div class="sixty">60%</div>
+      <div class="fifty">50%</div>
+      <div class="forty">40%</div>
+      <div class="thirty">30%</div>
+      <div class="twenty">20%</div>
+      <div class="ten">10%</div>
+      <div class="five">5%</div>
+    </section> 
+
+    <!-- Description Bar --> 
+    <section class="desc-bar collapsed">
+      <img src="assets/img/line.png" alt="lines" class="line1">
+      <p class="desc-text">Mauve is also called Analine Purple, Mauvaline and Mallow</p>
+      <img src="assets/img/line.png" alt="lines" class="line2">
+      <div class="down-arrow"></div>
     </section>
+
     
-    <!-- Info Icons - Bottom -->
-    <section class="bottom-icons clearfix">
-      <div class="left-sqs">
-        <div class="upper-left-sq">
-          <h3>6</h3>
+    <section class="icons">
+      <!-- Info Icons - Top -->
+      <section class="top-icons clearfix">
+        <div>
+          <img class="top-icon1" src="assets/img/mauve/icons/icon1.png" alt="">
+          <img class="top-icon2"src="assets/img/mauve/icons/icon2.png" alt="">
         </div>
-         <div class="upper-right-sq">
-          <div class="sm-sq1">30</div>
-          <div class="sm-sq2">0</div>
-          <div class="sm-sq3">100</div>
-          <div class="sm-sq4">0</div>
+          <p class="icon-text left-text1">first chem color</p>
+          <p class="icon-text right-text1">stylish</p>
+        <div>
         </div>
-        <div class="bottom-left-sq">
-          <h3>11</h3>
+         <div>
+          <img class="top-icon3" src="assets/img/mauve/icons/icon3.png" alt="">
+          <img class="top-icon4" src="assets/img/mauve/icons/icon4.png" alt="">
         </div>
-        <div class="bottom-right-sq">
-          <h3>16</h3>
+        <div>
+          <p class="icon-text left-text2">mournful</p>
+          <p class="icon-text right-text2">expensive</p>
         </div>
-      </div>
+      </section>
+      
+      <!-- Info Icons - Bottom -->
+      <section class="bottom-icons clearfix">
+        <div class="left-sqs">
+          <div class="upper-left-sq">
+            <h3>6</h3>
+          </div>
+           <div class="upper-right-sq">
+            <div class="sm-sq1">30</div>
+            <div class="sm-sq2">0</div>
+            <div class="sm-sq3">100</div>
+            <div class="sm-sq4">0</div>
+          </div>
+          <div class="bottom-left-sq">
+            <h3>11</h3>
+          </div>
+          <div class="bottom-right-sq">
+            <h3>16</h3>
+          </div>
+        </div>
 
-      <!-- Info Icons - Far Right Col -->
-      <div class="right-col">
-        <div class="a">178</div>
-        <div class="b">0</div>
-        <div class="c">255</div>
-      </div> 
+        <!-- Info Icons - Far Right Col -->
+        <div class="right-col">
+          <div class="a">178</div>
+          <div class="b">0</div>
+          <div class="c">255</div>
+        </div> 
+      </section>
+    </section> <!-- end icon section --> 
+
+    <section class="main-text">
+      <p>Mauve is the coolest hue in of all the reds. It has a fashionable and stylish appearance, particularly when paired with grey and other neutral colors. Mauve naturally appears more expensive than other colors. Mauvaline was the first color to be made with chemicals rather than natural pigments. Originally, the color was obtained from leaves of the mallow flower. Mauve is also a color representative of mourning, and brings a human touch to ceremonies covered in black. It is also a common choice for bridesmaid dresses, proving that women can be as bleak about their weddings as the grooms they drag down the aisle.</p>
     </section>
-  </section> <!-- end icon section --> 
 
-  <section class="main-text">
-    <p>Mauve is the coolest hue in of all the reds. It has a fashionable and stylish appearance, particularly when paired with grey and other neutral colors. Mauve naturally appears more expensive than other colors. Mauvaline was the first color to be made with chemicals rather than natural pigments. Originally, the color was obtained from leaves of the mallow flower. Mauve is also a color representative of mourning, and brings a human touch to ceremonies covered in black. It is also a common choice for bridesmaid dresses, proving that women can be as bleak about their weddings as the grooms they drag down the aisle.</p>
-  </section>
-
-  <section class="right-infographic">
-    <h1>nice glasses, <span>nerd</span></h1>
-    <img src="assets/img/infog-line.png" alt="">
-    <p>Looking at the color red for a long time can be exhausting and leads to near-sightedness.</p>
-    <img src="assets/img/infog-line.png" alt="">
-    <img class="glasses" src="assets/img/mauve/glasses.png" alt="">
-  </section> 
+    <section class="right-infographic">
+      <h1>nice glasses, <span>nerd</span></h1>
+      <img src="assets/img/infog-line.png" alt="">
+      <p>Looking at the color red for a long time can be exhausting and leads to near-sightedness.</p>
+      <img src="assets/img/infog-line.png" alt="">
+      <img class="glasses" src="assets/img/mauve/glasses.png" alt="">
+    </section> 
+</section> <!-- end main section --> 
 
 <?php include("footer.php"); ?>
