3d9041d6500b230f71d50874d11e5e8c6547a33c
kryss88ltj <christine.stauffer@gmail.com>
2014-06-15 20:03:54 -0400

    added nav on home page, scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,54 @@ $(document).ready(function() {
     $( ".line1, .line2").toggle();
   });
 
+  // Page Scrolling
+  
+  $(function() {
+    
+    $('section.scrollsections .title').hide(); // Hide all titles
+
+    $('section.scrollsections')
+      .scrollSections({
+        alwaysStartWithFirstSection : true, // force to load the page on the first section (prevent broswer caching)
+        
+        before: function($currentSection, $nextSection){
+
+          _initFuturSection($nextSection);
+        },
+        
+        after: function($currentSection, $previousSection){
+
+          _initNewSection($currentSection, $previousSection);
+        }
+      });
+
+    function _initNewSection($section, $prevSection){
+
+      // Do some stuff each time this new section ends animating
+      
+      // Only if there is a previous section (its null on first scroll)
+      if($prevSection){
+        $('.title', $prevSection).hide(); // Hide old section title
+      }
+      $('.title', $section).fadeIn(); // Fade in current section title
+      
+      // Do some stuffs only on first init
+      if( !$section.data('isInit') ){
+        // Create slider for example
+        // $('.selector', $section).slider();
+
+        // Singleton
+        $section.data('isInit', true);
+      }
+    }
+
+    function _initFuturSection($section){
+
+      // Do some stuff each time before this section appears
+      $('.title', $section).hide();
+    }
+  });
+
+
 });
+
--- header.php
+++ header.php
@@ -6,10 +6,14 @@
 		<link href="assets/css/bottom-nav.css" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" type="text/css" href="assets/css/<?php echo $css;?>.css">
     <link href="assets/css/homelink.css" rel="stylesheet" type="text/css" />
+   
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+   <!-- // <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
 		<script src="assets/js/scrollingcarousel.2.0.min.js"></script>
     <script src="assets/js/main.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/pagescroll.js"></script>
 
 	</head>
 	<body>
