aaca8b5ae46e703c14941644a1d362c11d7b1256
kryss88ltj <christine.stauffer@gmail.com>
2014-09-08 22:35:04 -0400

    added gold

---
+++ gold.php
@@ -0,0 +1,147 @@
+<?php $title = "Gold"; ?>
+<?php $css = "gold"; ?>
+
+<?php include("header.php"); ?>
+
+
+<!-- Main Section - Includes all but bottom nav slider -->
+<section class="main-section clearfix">
+
+  <!-- Return Home -->
+  <a href="index.php"><img class="home" src="assets/img/home/eye.gif" alt="home"></a>
+
+  <!-- Side Menu Left -->
+  <nav class="left-nav">
+    <a href="amber.php">
+      <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h5>if you're happy and you know it</h5>
+      <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h4>amber</h4>
+      <img class="icon" src="assets/img/nav/amber-icon.png" alt="amber icon">
+    </a>
+  </nav>
+
+
+  <section class="main-middle-section clearfix">
+   <!-- Color Title --> 
+    <h1 class="title">gold</h1>
+
+    <!-- Bourges Number --> 
+    <div class="bourgesBlock">
+      <h5 class="bourges">bourges</h5>
+      <h5 class="numeral">9</h5>
+      <h5 class="number">number</h5>
+    </div> 
+
+    <!-- Percentage Bar --> 
+    <section class="percentageBar">
+      <div class="top-border"></div>
+      <div class="ninety">90%</div>
+      <div class="eighty">80%</div>
+      <div class="seventy">70%</div>
+      <div class="sixty">60%</div>
+      <div class="fifty">50%</div>
+      <div class="forty">40%</div>
+      <div class="thirty">30%</div>
+      <div class="twenty">20%</div>
+      <div class="ten">10%</div>
+      <div class="five">5%</div>
+    </section> 
+
+    <!-- Description Bar --> 
+    <section class="desc-bar collapsed">
+      <img src="assets/img/line.png" alt="lines" class="line1">
+      <p class="desc-text">If you like this hue, you might be a gold digger.</p>
+      <img src="assets/img/line.png" alt="lines" class="line2">
+      <div class="down-arrow"></div>
+    </section>
+
+    
+    <section class="icons">
+      <!-- Info Icons - Top -->
+ 
+      <section class="top-icons clearfix">
+        <div class="bottom-left-iconset">
+          <img class="top-icon3" src="assets/img/gold/icons/icon3.png" alt="">
+          <p class="icon-text left-text2">abundance (for men)</p>
+        </div>
+        <div class="top-right-iconset">
+          <img class="top-icon2" src="assets/img/gold/icons/icon2.png" alt="">
+          <p class="icon-text right-text1">wisdom (when metallic)</p>
+        </div>
+        <div class="bottom-right-iconset">
+          <img class="top-icon4" src="assets/img/gold/icons/icon4.png" alt="">
+          <p class="icon-text right-text2">nirvana</p>
+        </div>
+        <div class="top-left-iconset">
+          <img class="top-icon1" src="assets/img/gold/icons/icon1.png" alt="">
+          <p class="icon-text left-text1">phat stacks</p>
+        </div>
+      </section>
+
+      
+      <!-- Info Icons - Bottom -->
+      <section class="bottom-icons clearfix">
+        <div class="left-sqs">
+          <div class="upper-left-sq">
+            <h3><a href="emerald.php">14</a></h3>
+          </div>
+           <div class="upper-right-sq">
+            <div class="sm-sq1">0</div>
+            <div class="sm-sq2">20</div>
+            <div class="sm-sq3">100</div>
+            <div class="sm-sq4">0</div>
+          </div>
+          <div class="bottom-left-sq">
+            <h3><a href="violet.php">19</a></h3>
+          </div>
+          <div class="bottom-right-sq">
+            <h3><a href="scarlet.php">4</a></h3>
+          </div>
+        </div>
+
+        <!-- Info Icons - Far Right Col -->
+        <div class="right-col">
+          <div class="a">255</div>
+          <div class="b">77</div>
+          <div class="c">0</div>
+        </div> 
+      </section>
+    </section> <!-- end icon section --> 
+
+    <section class="main-text clearfix">
+      Ah, gold. The color of riches, fame, and glory. It's also the color of heaven in Japan, and indicative of the Buddha. It carries with it a degree of uncertainty, particularly in times of economic recession. A flat gold is a particularly popular accent color, but it is less likely to be used when the economy is bad. For men, gold is a color of abundance. When this gold is metallic, it is particularly wise and representative of higher ideals. Oh, and did I mention it indicates wealth?
+    </section>
+        
+    <section class="right-infographic">
+      
+      
+      <img src="assets/img/gold/right-info-pic.png" alt="">
+    </section> 
+  </section> <!-- end main middle section -->
+
+  <!-- Side Menu Right -->
+  <nav class="right-nav">
+    <a href="gold.php">
+      <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h5>ain't nothing yella 'bout it</h5>
+      <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h4>yellow</h4>
+      <img class="right-nav-icon" src="assets/img/nav/yellow-icon.png" alt="yellow icon">
+    </a>
+  </nav>
+
+</section> <!-- end entire main section --> 
+
+<section class="page-break">
+  <img src="assets/img/gold/arrows-transparent.png">
+</section>
+
+<script>
+    $('.main-text').flowtype({
+       maxFont   : 20,
+       fontRatio : 26});
+  </script>
+
+<?php include("footer.php"); ?>
+
---
+++ orange.php
@@ -0,0 +1,152 @@
+<?php $title = "Orange"; ?>
+<?php $css = "orange"; ?>
+
+<?php include("header.php"); ?>
+
+
+<!-- Main Section - Includes all but bottom nav slider -->
+<section class="main-section clearfix">
+
+  <!-- Return Home -->
+  <a href="index.php"><img class="home" src="assets/img/home/eye.gif" alt="home"></a>
+
+  <!-- Side Menu Left -->
+  <nav class="left-nav">
+    <a href="coral.php">
+      <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h5>it's getting hot in here, but leave on all your clothes</h5>
+      <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h4>coral</h4>
+      <img class="icon" src="assets/img/nav/coral-icon.png" alt="coral icon">
+    </a>
+  </nav>
+
+
+  <section class="main-middle-section clearfix">
+   <!-- Color Title --> 
+    <h1 class="title">orange</h1>
+
+    <!-- Bourges Number --> 
+    <div class="bourgesBlock">
+      <h5 class="bourges">bourges</h5>
+      <h5 class="numeral">7</h5>
+      <h5 class="number">number</h5>
+    </div> 
+
+    <!-- Percentage Bar --> 
+    <section class="percentageBar">
+      <div class="top-border"></div>
+      <div class="ninety">90%</div>
+      <div class="eighty">80%</div>
+      <div class="seventy">70%</div>
+      <div class="sixty">60%</div>
+      <div class="fifty">50%</div>
+      <div class="forty">40%</div>
+      <div class="thirty">30%</div>
+      <div class="twenty">20%</div>
+      <div class="ten">10%</div>
+      <div class="five">5%</div>
+    </section> 
+
+    <!-- Description Bar --> 
+    <section class="desc-bar collapsed">
+      <img src="assets/img/line.png" alt="lines" class="line1">
+      <p class="desc-text">Orange you glad you clicked that button?</p>
+      <img src="assets/img/line.png" alt="lines" class="line2">
+      <div class="down-arrow"></div>
+    </section>
+
+    
+    <section class="icons">
+      <!-- Info Icons - Top -->
+ 
+      <section class="top-icons clearfix">
+        <div class="bottom-left-iconset">
+          <img class="top-icon3" src="assets/img/orange/icons/icon3.png" alt="">
+          <p class="icon-text left-text2">courageous</p>
+        </div>
+        <div class="top-right-iconset">
+          <img class="top-icon2" src="assets/img/orange/icons/icon2.png" alt="">
+          <p class="icon-text right-text1">inspires creativity</p>
+        </div>
+        <div class="bottom-right-iconset">
+          <img class="top-icon4" src="assets/img/orange/icons/icon4.png" alt="">
+          <p class="icon-text right-text2">a great anti-depressant</p>
+        </div>
+        <div class="top-left-iconset">
+          <img class="top-icon1" src="assets/img/orange/icons/icon1.png" alt="">
+          <p class="icon-text left-text1">hoo yeah! enthusiasm</p>
+        </div>
+      </section>
+
+      
+      <!-- Info Icons - Bottom -->
+      <section class="bottom-icons clearfix">
+        <div class="left-sqs">
+          <div class="upper-left-sq">
+            <h3><a href="leaf-green.php">12</a></h3>
+          </div>
+           <div class="upper-right-sq">
+            <div class="sm-sq1">0</div>
+            <div class="sm-sq2">70</div>
+            <div class="sm-sq3">100</div>
+            <div class="sm-sq4">0</div>
+          </div>
+          <div class="bottom-left-sq">
+            <h3><a href="sky-blue.php">17</a></h3>
+          </div>
+          <div class="bottom-right-sq">
+            <h3><a href="magenta.php">2</a></h3>
+          </div>
+        </div>
+
+        <!-- Info Icons - Far Right Col -->
+        <div class="right-col">
+          <div class="a">255</div>
+          <div class="b">77</div>
+          <div class="c">0</div>
+        </div> 
+      </section>
+    </section> <!-- end icon section --> 
+
+    <section class="main-text clearfix">
+      Orange is a objectively the life of the party. It is witty, humorous, courageous, outgoing and always seems to have a really great story. Sometimes it can be misunderstood, but orange is unmistakably ambitious. Orange was the color of choice for ancient Roman armies and 19th century brides in London. It's the color of hindu priests and protestants in Ireland. Heck, add a little brown or deep red to your orange and just about everyone will like it! Orange is reassuring and tender, but too much orange can feel irresponsible. Physically, Orange stimulates oxygen flow to the brain. It spurs creativity and enthusiasm. It's even a good antidepressant and appetite stimulant, except when you're stressed. When you're stressed out, orange is that annoyingly positive person at the party who won't leave you alone.
+    </section>
+        
+    <section class="right-infographic">
+      <h1>what's in a</h1> 
+      <span>name?</span>
+      <img src="assets/img/infog-line.png" alt="">
+      <p>Ancient Romans used the term orange
+        to refer to all fruits, which roughly
+        translates to &#34;food of the gods.&#34;</p>
+      <img src="assets/img/infog-line.png" alt="">
+      <img class="half-orange-icon" src="assets/img/orange/half-orange-icon.png" alt="">
+    </section> 
+  </section> <!-- end main middle section -->
+
+  <!-- Side Menu Right -->
+  <nav class="right-nav">
+    <a href="amber.php">
+      <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h5>if you're happy and you know it</h5>
+      <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
+      <h4>amber</h4>
+      <img class="right-nav-icon" src="assets/img/nav/amber-icon.png" alt="amber icon">
+    </a>
+  </nav>
+
+</section> <!-- end entire main section --> 
+
+<section class="page-break">
+  <img src="assets/img/orange/arrows-transparent.png">
+</section>
+
+<script>
+    $('.main-text').flowtype({
+       maxFont   : 20,
+       fontRatio : 26});
+  </script>
+
+<?php include("footer.php"); ?>
+
