61abeba08d883ea3ca01f52458afc3043f50ae85
kryss88ltj <christine.stauffer@gmail.com>
2014-09-09 20:15:05 -0400

    more stuff

--- lime.php
+++ lime.php
@@ -1,5 +1,5 @@
-<?php $title = "Gold"; ?>
-<?php $css = "gold"; ?>
+<?php $title = "Lime"; ?>
+<?php $css = "lime"; ?>
 
 <?php include("header.php"); ?>
 
@@ -12,24 +12,24 @@
 
   <!-- Side Menu Left -->
   <nav class="left-nav">
-    <a href="amber.php">
+    <a href="yellow.php">
       <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h5>if you're happy and you know it</h5>
+      <h5>rich like scrooge (mcduck or ebenezer)</h5>
       <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h4>amber</h4>
-      <img class="icon" src="assets/img/nav/amber-icon.png" alt="amber icon">
+      <h4>yellow</h4>
+      <img class="icon" src="assets/img/nav/yellow-icon.png" alt="yellow icon">
     </a>
   </nav>
 
 
   <section class="main-middle-section clearfix">
    <!-- Color Title --> 
-    <h1 class="title">gold</h1>
+    <h1 class="title">lime</h1>
 
     <!-- Bourges Number --> 
     <div class="bourgesBlock">
       <h5 class="bourges">bourges</h5>
-      <h5 class="numeral">9</h5>
+      <h5 class="numeral">11</h5>
       <h5 class="number">number</h5>
     </div> 
 
@@ -51,7 +51,7 @@
     <!-- Description Bar --> 
     <section class="desc-bar collapsed">
       <img src="assets/img/line.png" alt="lines" class="line1">
-      <p class="desc-text">If you like this hue, you might be a gold digger.</p>
+      <p class="desc-text">Named for the lesser half of lemon-lime soda.</p>
       <img src="assets/img/line.png" alt="lines" class="line2">
       <div class="down-arrow"></div>
     </section>
@@ -61,21 +61,21 @@
       <!-- Info Icons - Top -->
  
       <section class="top-icons clearfix">
-        <div class="bottom-left-iconset">
-          <img class="top-icon3" src="assets/img/gold/icons/icon3.png" alt="">
-          <p class="icon-text left-text2">abundance (for men)</p>
+        <div class="top-left-iconset">
+          <img class="top-icon1" src="assets/img/lime/icons/icon1.png" alt="">
+          <p class="icon-text left-text1">suburban</p>
         </div>
         <div class="top-right-iconset">
-          <img class="top-icon2" src="assets/img/gold/icons/icon2.png" alt="">
-          <p class="icon-text right-text1">wisdom (when metallic)</p>
+          <img class="top-icon2" src="assets/img/lime/icons/icon2.png" alt="">
+          <p class="icon-text right-text1">clean</p>
         </div>
-        <div class="bottom-right-iconset">
-          <img class="top-icon4" src="assets/img/gold/icons/icon4.png" alt="">
-          <p class="icon-text right-text2">nirvana</p>
+        <div class="bottom-left-iconset">
+          <img class="top-icon3" src="assets/img/lime/icons/icon3.png" alt="">
+          <p class="icon-text left-text2">simple</p>
         </div>
-        <div class="top-left-iconset">
-          <img class="top-icon1" src="assets/img/gold/icons/icon1.png" alt="">
-          <p class="icon-text left-text1">phat stacks</p>
+         <div class="bottom-right-iconset">
+          <img class="top-icon4" src="assets/img/lime/icons/icon4.png" alt="">
+          <p class="icon-text right-text2">gentle</p>
         </div>
       </section>
 
@@ -84,19 +84,19 @@
       <section class="bottom-icons clearfix">
         <div class="left-sqs">
           <div class="upper-left-sq">
-            <h3><a href="emerald.php">14</a></h3>
+            <h3><a href="cyan.php">16</a></h3>
           </div>
            <div class="upper-right-sq">
-            <div class="sm-sq1">0</div>
-            <div class="sm-sq2">20</div>
+            <div class="sm-sq1">70</div>
+            <div class="sm-sq2">0</div>
             <div class="sm-sq3">100</div>
             <div class="sm-sq4">0</div>
           </div>
           <div class="bottom-left-sq">
-            <h3><a href="violet.php">19</a></h3>
+            <h3><a href="mauve.php">1</a></h3>
           </div>
           <div class="bottom-right-sq">
-            <h3><a href="scarlet.php">4</a></h3>
+            <h3><a href="coral.php">6</a></h3>
           </div>
         </div>
 
@@ -110,37 +110,42 @@
     </section> <!-- end icon section --> 
 
     <section class="main-text clearfix">
-      Ah, gold. The color of riches, fame, and glory. It's also the color of heaven in Japan, and indicative of the Buddha. It carries with it a degree of uncertainty, particularly in times of economic recession. A flat gold is a particularly popular accent color, but it is less likely to be used when the economy is bad. For men, gold is a color of abundance. When this gold is metallic, it is particularly wise and representative of higher ideals. Oh, and did I mention it indicates wealth?
+      Lime green is a country, suburban green. It is young, fresh, naive, and even tasty. Studies found that cherry Jell-o is preferred by males when it was lime green. Lime represents a simpler time - it isn't elegant, but lime is okay with that. It is a gentle color that would gladly share it's lunch with you. Lime is frequently used in fast food and was employed by BP to represent a clean and environmental company. That was working very well for them, until their big oil spill in 2010. Sorry, BP, there's only so much a color can do for your PR image.
     </section>
         
     <section class="right-infographic">
-      
-      
-      <img src="assets/img/gold/right-info-pic.png" alt="">
+      <h1>GREEN, GREEN,</h1> 
+      <span>GREEEEEN</span>
+      <img class="linetop" src="assets/img/infog-line.png" alt="">
+      <p>There are more different greens than any other color. Green is a proven stress reliever. Green is a good 
+      and kind color. Green is the best background for presentations. 
+      Green spawns new ideas. Green makes men feel weak. Green helps you accomplish tasks on time. Time moves quicker in green. Green
+      is a wonderful parent to the kids.</p>
+      <img class="linebottom" src="assets/img/infog-line.png" alt="">
     </section> 
   </section> <!-- end main middle section -->
 
   <!-- Side Menu Right -->
   <nav class="right-nav">
-    <a href="yellow.php">
+    <a href="sea-green.php">
       <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h5>ain't nothing yella 'bout it</h5>
+      <h5>named for the color of the ocean floor</h5>
       <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h4>yellow</h4>
-      <img class="right-nav-icon" src="assets/img/nav/yellow-icon.png" alt="yellow icon">
+      <h4>sea green</h4>
+      <img class="right-nav-icon" src="assets/img/nav/sea-green-icon.png" alt="sea green icon">
     </a>
   </nav>
 
 </section> <!-- end entire main section --> 
 
 <section class="page-break">
-  <img src="assets/img/gold/arrows-transparent.png">
+  <img src="assets/img/lime/arrows-transparent.png">
 </section>
 
-<script>
+  <script>
     $('.main-text').flowtype({
        maxFont   : 20,
-       fontRatio : 26});
+       fontRatio : 20});
   </script>
 
 <?php include("footer.php"); ?>
--- yellow.php
+++ yellow.php
@@ -1,5 +1,5 @@
-<?php $title = "Gold"; ?>
-<?php $css = "gold"; ?>
+<?php $title = "Yellow"; ?>
+<?php $css = "yellow"; ?>
 
 <?php include("header.php"); ?>
 
@@ -12,24 +12,24 @@
 
   <!-- Side Menu Left -->
   <nav class="left-nav">
-    <a href="amber.php">
+    <a href="gold.php">
       <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h5>if you're happy and you know it</h5>
+      <h5>rich like scrooge or ebenezer</h5>
       <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h4>amber</h4>
-      <img class="icon" src="assets/img/nav/amber-icon.png" alt="amber icon">
+      <h4>gold</h4>
+      <img class="icon" src="assets/img/nav/gold-icon.png" alt="gold icon">
     </a>
   </nav>
 
 
   <section class="main-middle-section clearfix">
    <!-- Color Title --> 
-    <h1 class="title">gold</h1>
+    <h1 class="title">yellow</h1>
 
     <!-- Bourges Number --> 
     <div class="bourgesBlock">
       <h5 class="bourges">bourges</h5>
-      <h5 class="numeral">9</h5>
+      <h5 class="numeral">10</h5>
       <h5 class="number">number</h5>
     </div> 
 
@@ -51,7 +51,7 @@
     <!-- Description Bar --> 
     <section class="desc-bar collapsed">
       <img src="assets/img/line.png" alt="lines" class="line1">
-      <p class="desc-text">If you like this hue, you might be a gold digger.</p>
+      <p class="desc-text">Yellow is the fattest color of all, and probably should stop eating so many carbs.</p>
       <img src="assets/img/line.png" alt="lines" class="line2">
       <div class="down-arrow"></div>
     </section>
@@ -62,20 +62,20 @@
  
       <section class="top-icons clearfix">
         <div class="bottom-left-iconset">
-          <img class="top-icon3" src="assets/img/gold/icons/icon3.png" alt="">
-          <p class="icon-text left-text2">abundance (for men)</p>
+          <img class="top-icon3" src="assets/img/yellow/icons/icon3.png" alt="">
+          <p class="icon-text left-text2">largest hue</p>
         </div>
         <div class="top-right-iconset">
-          <img class="top-icon2" src="assets/img/gold/icons/icon2.png" alt="">
-          <p class="icon-text right-text1">wisdom (when metallic)</p>
+          <img class="top-icon2" src="assets/img/yellow/icons/icon2.png" alt="">
+          <p class="icon-text right-text1">boosts heart rate</p>
         </div>
         <div class="bottom-right-iconset">
-          <img class="top-icon4" src="assets/img/gold/icons/icon4.png" alt="">
-          <p class="icon-text right-text2">nirvana</p>
+          <img class="top-icon4" src="assets/img/yellow/icons/icon4.png" alt="">
+          <p class="icon-text right-text2">hard to write</p>
         </div>
         <div class="top-left-iconset">
-          <img class="top-icon1" src="assets/img/gold/icons/icon1.png" alt="">
-          <p class="icon-text left-text1">phat stacks</p>
+          <img class="top-icon1" src="assets/img/yellow/icons/icon1.png" alt="">
+          <p class="icon-text left-text1">sunny and bright</p>
         </div>
       </section>
 
@@ -84,57 +84,61 @@
       <section class="bottom-icons clearfix">
         <div class="left-sqs">
           <div class="upper-left-sq">
-            <h3><a href="emerald.php">14</a></h3>
+            <h3><a href="teal.php">15</a></h3>
           </div>
            <div class="upper-right-sq">
             <div class="sm-sq1">0</div>
-            <div class="sm-sq2">20</div>
+            <div class="sm-sq2">70</div>
             <div class="sm-sq3">100</div>
             <div class="sm-sq4">0</div>
           </div>
           <div class="bottom-left-sq">
-            <h3><a href="violet.php">19</a></h3>
+            <h3><a href="purple.php">20</a></h3>
           </div>
           <div class="bottom-right-sq">
-            <h3><a href="scarlet.php">4</a></h3>
+            <h3><a href="poster-red.php">5</a></h3>
           </div>
         </div>
 
         <!-- Info Icons - Far Right Col -->
         <div class="right-col">
           <div class="a">255</div>
-          <div class="b">77</div>
+          <div class="b">255</div>
           <div class="c">0</div>
         </div> 
       </section>
     </section> <!-- end icon section --> 
 
     <section class="main-text clearfix">
-      Ah, gold. The color of riches, fame, and glory. It's also the color of heaven in Japan, and indicative of the Buddha. It carries with it a degree of uncertainty, particularly in times of economic recession. A flat gold is a particularly popular accent color, but it is less likely to be used when the economy is bad. For men, gold is a color of abundance. When this gold is metallic, it is particularly wise and representative of higher ideals. Oh, and did I mention it indicates wealth?
+      Yellow is the biggest of all the hues - it is large and in charge. It is an optimistic, self-determined hue and represents hope. Just looking at yellow will make someone happy and is especially good for short-term
+      activities. However, just like poster red it can be physically tiring in long term activities. Yellow is a confident color, but in small quantities can seem irrational. Too much yellow will give you an ego and increase your heart rate. If your favorite color is yellow, you’re probably original and slightly perfectionistic. Yellow is also a tad schizophrenic, which might be why people either love it or hate it. Yellow can be both sour and inspiring. It can be anxious, sharp or bitter. It can open up any space, making the page look physically bigger. Yellow is the brightest color, but also hardest to read on a white page. It is extremely hard to use as a background color.
     </section>
         
     <section class="right-infographic">
-      
-      
-      <img src="assets/img/gold/right-info-pic.png" alt="">
+      <h1>cause and effect</h1> 
+      <img src="assets/img/infog-line.png" alt="">
+      <p>Depending on how much green or red is 
+      in a yellow, it can carry different emotional responses.</p>
+      <img src="assets/img/infog-line.png" alt="">
+      <img class="yellow-info-icon" src="assets/img/yellow/yellow-info-icon.png" alt="">
     </section> 
   </section> <!-- end main middle section -->
 
   <!-- Side Menu Right -->
   <nav class="right-nav">
-    <a href="yellow.php">
+    <a href="lime.php">
       <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h5>ain't nothing yella 'bout it</h5>
+      <h5>so fresh and so clean clean</h5>
       <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h4>yellow</h4>
-      <img class="right-nav-icon" src="assets/img/nav/yellow-icon.png" alt="yellow icon">
+      <h4>lime</h4>
+      <img class="right-nav-icon" src="assets/img/nav/lime-icon.png" alt="lime icon">
     </a>
   </nav>
 
 </section> <!-- end entire main section --> 
 
 <section class="page-break">
-  <img src="assets/img/gold/arrows-transparent.png">
+  <img src="assets/img/coral/arrows-transparent.png">
 </section>
 
 <script>
