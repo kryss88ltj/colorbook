2ff94f2a638f602b7e5feed195e5e227459f2ba5
kryss88ltj <christine.stauffer@gmail.com>
2014-09-10 08:32:32 -0400

    fixed bottom nav, added leaf green

--- leaf-green.php
+++ leaf-green.php
@@ -1,5 +1,5 @@
-<?php $title = "Lime"; ?>
-<?php $css = "lime"; ?>
+<?php $title = "Leaf Green"; ?>
+<?php $css = "leaf-green"; ?>
 
 <?php include("header.php"); ?>
 
@@ -12,24 +12,24 @@
 
   <!-- Side Menu Left -->
   <nav class="left-nav">
-    <a href="yellow.php">
+    <a href="lime.php">
       <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h5>rich like scrooge (mcduck or ebenezer)</h5>
+      <h5>so fresh and so clean clean</h5>
       <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h4>yellow</h4>
-      <img class="icon" src="assets/img/nav/yellow-icon.png" alt="yellow icon">
+      <h4>lime</h4>
+      <img class="icon" src="assets/img/nav/lime-icon.png" alt="yellow icon">
     </a>
   </nav>
 
 
   <section class="main-middle-section clearfix">
    <!-- Color Title --> 
-    <h1 class="title">lime</h1>
+    <h1 class="title">leaf green</h1>
 
     <!-- Bourges Number --> 
     <div class="bourgesBlock">
       <h5 class="bourges">bourges</h5>
-      <h5 class="numeral">11</h5>
+      <h5 class="numeral">12</h5>
       <h5 class="number">number</h5>
     </div> 
 
@@ -51,7 +51,7 @@
     <!-- Description Bar --> 
     <section class="desc-bar collapsed">
       <img src="assets/img/line.png" alt="lines" class="line1">
-      <p class="desc-text">Named for the lesser half of lemon-lime soda.</p>
+      <p class="desc-text">Leaf green is also known as grass green and collared greens.</p>
       <img src="assets/img/line.png" alt="lines" class="line2">
       <div class="down-arrow"></div>
     </section>
@@ -62,20 +62,20 @@
  
       <section class="top-icons clearfix">
         <div class="top-left-iconset">
-          <img class="top-icon1" src="assets/img/lime/icons/icon1.png" alt="">
-          <p class="icon-text left-text1">suburban</p>
+          <img class="top-icon1" src="assets/img/leaf-green/icons/icon1.png" alt="">
+          <p class="icon-text left-text1">clean</p>
         </div>
         <div class="top-right-iconset">
-          <img class="top-icon2" src="assets/img/lime/icons/icon2.png" alt="">
-          <p class="icon-text right-text1">clean</p>
+          <img class="top-icon2" src="assets/img/leaf-green/icons/icon2.png" alt="">
+          <p class="icon-text right-text1">healing</p>
         </div>
         <div class="bottom-left-iconset">
-          <img class="top-icon3" src="assets/img/lime/icons/icon3.png" alt="">
-          <p class="icon-text left-text2">simple</p>
+          <img class="top-icon3" src="assets/img/leaf-green/icons/icon3.png" alt="">
+          <p class="icon-text left-text2">eco-friendly</p>
         </div>
          <div class="bottom-right-iconset">
-          <img class="top-icon4" src="assets/img/lime/icons/icon4.png" alt="">
-          <p class="icon-text right-text2">gentle</p>
+          <img class="top-icon4" src="assets/img/leaf-green/icons/icon4.png" alt="">
+          <p class="icon-text right-text2">corrective</p>
         </div>
       </section>
 
@@ -84,19 +84,19 @@
       <section class="bottom-icons clearfix">
         <div class="left-sqs">
           <div class="upper-left-sq">
-            <h3><a href="cyan.php">16</a></h3>
+            <h3><a href="sky-blue.php">17</a></h3>
           </div>
            <div class="upper-right-sq">
-            <div class="sm-sq1">70</div>
+            <div class="sm-sq1">100</div>
             <div class="sm-sq2">0</div>
             <div class="sm-sq3">100</div>
             <div class="sm-sq4">0</div>
           </div>
           <div class="bottom-left-sq">
-            <h3><a href="mauve.php">1</a></h3>
+            <h3><a href="magenta.php">2</a></h3>
           </div>
           <div class="bottom-right-sq">
-            <h3><a href="coral.php">6</a></h3>
+            <h3><a href="orange.php">7</a></h3>
           </div>
         </div>
 
@@ -110,36 +110,32 @@
     </section> <!-- end icon section --> 
 
     <section class="main-text clearfix">
-      Lime green is a country, suburban green. It is young, fresh, naive, and even tasty. Studies found that cherry Jell-o is preferred by males when it was lime green. Lime represents a simpler time - it isn't elegant, but lime is okay with that. It is a gentle color that would gladly share it's lunch with you. Lime is frequently used in fast food and was employed by BP to represent a clean and environmental company. That was working very well for them, until their big oil spill in 2010. Sorry, BP, there's only so much a color can do for your PR image.
+      <p>Leaf green is the sexy nurse of the color world - She's compassionate, kind and represents healing and growth. It is an extremely clean color, particularly in the US. Leaf green has the ability to make a package look
+      eco-friendly and environmental. When mixed with a little bit of black, leaf green appears fat-free. Leaf green is also the color of correction and fixing mistakes.</p>
+
+      <p class="para-2">Story time: The Blackfriar bridge in London was a popular jumping spot for suicidal englishmen. It got so bad that they decided to paint the black, desolate bridge a more refreshing shade of green. They hoped to take advantage of the healing and stress-reducing nature of green.</p> 
     </section>
         
     <section class="right-infographic">
-      <h1>GREEN, GREEN,</h1> 
-      <span>GREEEEEN</span>
-      <img class="linetop" src="assets/img/infog-line.png" alt="">
-      <p>There are more different greens than any other color. Green is a proven stress reliever. Green is a good 
-      and kind color. Green is the best background for presentations. 
-      Green spawns new ideas. Green makes men feel weak. Green helps you accomplish tasks on time. Time moves quicker in green. Green
-      is a wonderful parent to the kids.</p>
-      <img class="linebottom" src="assets/img/infog-line.png" alt="">
+      <img class="right-info-img" src="assets/img/leafgreen/leafgreen-right-infopic.png" alt="">
     </section> 
   </section> <!-- end main middle section -->
 
   <!-- Side Menu Right -->
   <nav class="right-nav">
-    <a href="leaf-green.php">
+    <a href="sea-green.php">
       <img class="top-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h5>saving the planet, one hue at a time</h5>
+      <h5>see?  green!  (hur hur hur)</h5>
       <img class="bottom-title-line" src="assets/img/nav/nav-title-line.png" alt="title line">
-      <h4>leaf green</h4>
-      <img class="right-nav-icon" src="assets/img/nav/leaf-green-icon.png" alt="leaf green icon">
+      <h4>sea green</h4>
+      <img class="right-nav-icon" src="assets/img/nav/sea-green-icon.png" alt="sea   green icon">
     </a>
   </nav>
 
 </section> <!-- end entire main section --> 
 
 <section class="page-break">
-  <img src="assets/img/lime/arrows-transparent.png">
+  <img src="assets/img/sea-green/arrows-transparent.png">
 </section>
 
   <script>
